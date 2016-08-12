fb155f283b41fb1abcaa3968e671673d93b9492f
ikruiper <ikruiper@ymail.com>
2016-08-12 12:05:32 +0100

    adding studentSendPoints interactions

--- routes/studentDisplayClasses.js
+++ routes/studentDisplayClasses.js
@@ -3,15 +3,15 @@
  */
 var express = require('express');
 var router = express.Router();
-var classesTable = require('../classes.json');
-
+var ethClasses = require('../lib/classes.js');
 
 /* Get instructor-dashboard page. */
 router.get('/', function(req, res, next){
     res.render('studentDisplayClasses', {
         header: 'Student Display Classes' ,
-        classes: classesTable
-
+        // TODO: re-enable the getClassesFor(user) once the enrollment is working
+        // classes: ethClasses.getClassesFor(req.session.authenticatedUser)
+        classes: ethClasses.getAllClasses()
     });
 });
 
--- routes/studentSendPoints.js
+++ routes/studentSendPoints.js
@@ -4,14 +4,25 @@
 var express = require('express');
 var router = express.Router();
 var inst = require('../instructor.json');
-
+var ethClasses = require('../lib/classes.js');
 
 /* Get instructor-dashboard page. */
 router.get('/', function(req, res, next){
     res.render('studentSendPoints', {
         header: 'Student Send Points' ,
-        instructor: inst
-
+        instructor: inst,
+        //TODO: hook this to the ethClass and remove hard-coded class sample
+        // class: ethClasses.getClassForAddress(req.query.classId)
+        theClass: {
+            classId: "0xfdccaa886d3dfdf1a157ad605ea4185d2f420a52",
+            name: "Networks",
+            location: "Room 1",
+            startTime: "09:00",
+            duration: 1,
+            date: "14/08/2016",
+            instructor: "instructor1@qub.ac.uk",
+            classType: "lecture"
+        }
     });
 });
 
