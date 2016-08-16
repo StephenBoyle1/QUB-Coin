13a0ecc9e90c0177809541f27790cf859a12b902
linziroberts <lroberts04@qub.ac.uk>
2016-08-16 11:42:00 +0100

    Added functionality for check boxes and radio buttons on the adminClassesCreated, adminEnrollStudent, adminStudentsEnrolled pages.

--- routes/adminEnrollStudent.js
+++ routes/adminEnrollStudent.js
@@ -5,13 +5,27 @@ var express = require('express');
 var router = express.Router();
 var studentsTable = require('../students.json');
 var classesTable = require('../classes.json');
-//var html = new EJS({url: 'instructorDashboard.ejs'}).render(data);
+var ethClasses = require('../lib/classes.js');
 
 /* Get adminClassesCreated page. */
 router.get('/', function(req, res){
     res.render('adminEnrollStudent', {
         students: studentsTable,
-        classes: classesTable
+        classes: classesTable,
+
+        header: 'Admin Enroll Student',
+        //TODO: hook this to the ethClass and remove hard-coded class sample
+        // class: ethClasses.getClassForAddress(req.query.classId)
+        theClass: {
+            classId: "0xfdccaa886d3dfdf1a157ad605ea4185d2f420a52",
+            name: "Programming",
+            location: "Room 1",
+            startTime: "09:00",
+            duration: 1,
+            date: "14/08/2016",
+            instructor: "instructor1@qub.ac.uk",
+            classType: "lecture"
+        }
 
     });
 });
--- routes/adminStudentsEnrolled.js
+++ routes/adminStudentsEnrolled.js
@@ -4,13 +4,17 @@
 var express = require('express');
 var router = express.Router();
 var studentsTable = require('../students.json');
+var ethClasses = require('../lib/classes.js');
 //var html = new EJS({url: 'instructorDashboard.ejs'}).render(data);
 
 /* Get adminClassesCreated page. */
 router.get('/', function(req, res){
     res.render('adminStudentsEnrolled', {
-        students: studentsTable
-
+        students: studentsTable,
+        header: 'Admin Enroll Student',
+        theStudents: {
+            name: "Linzi Roberts"
+        }
     });
 });
 
