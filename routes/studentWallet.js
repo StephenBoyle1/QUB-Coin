3dce404e12eee78f2f332cc6c8ce7b93c37d080e
ikruiper <ikruiper@ymail.com>
2016-09-01 15:06:42 +0100

    Fixed refreshed view for studentWallet balances

--- routes/studentWallet.js
+++ routes/studentWallet.js
@@ -1,17 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var ethClasses = require('../lib/classes.js');
+var ethAuth = require('../lib/auth.js');
 var transactionHistoryTable = require('../transactionHistory.json');
 
 
 /* Get instructor-dashboard page. */
 router.get('/', function (req, res, next) {
     if (req.session.authenticatedUsername && req.session.authenticatedUsername != '') {
+        var refreshedAuthenticatedUser = ethAuth.callGetUser(req.session.authenticatedUser.accountId);
         res.render('studentWallet', {
             header: 'Student Wallet',
-            authenticatedUser: req.session.authenticatedUser,
+            authenticatedUser: refreshedAuthenticatedUser,
             classes: transactionHistoryTable,
-            transfers: ethClasses.getTransfersFor(req.session.authenticatedUser)
+            transfers: ethClasses.getTransfersFor(refreshedAuthenticatedUser)
         });
     } else {
 
